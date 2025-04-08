f8119ae9ff16d19e58831caa4e58399601e8eb83
Ayesha Siddiqua <ayeshasiddiqua.iubat@gmail.com>
2025-04-08 11:18:58 +0600

    Create Database

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Online_Medicine_Donation.Controllers
+{
+
+    public class RegistrationController : Controller
+    {
+        public IActionResult NGORegister()
+        {
+            return View();
+        }        
+        public IActionResult UserRegister()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/OnlineMedicineContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Online_Medicine_Donation.Areas.NGO.Models;
+using Online_Medicine_Donation.Areas.User.Models;
+
+namespace Online_Medicine_Donation.Data
+{
+    public class OnlineMedicineContext : DbContext
+    {
+        public OnlineMedicineContext(DbContextOptions<OnlineMedicineContext> options)
+            : base(options)
+        {
+
+        }
+    }
+
+}
